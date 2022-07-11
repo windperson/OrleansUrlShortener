a7dd3103167a43da7e13bf86675a04de75e336fb
windperson <windperson@gmail.com>
2022-07-11 18:17:50 +0800

    Init repo

---
+++ src/UrlShortener.Backend.SiloHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UrlShortener.Backend.SiloHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
