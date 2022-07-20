a94596a16345b0a782b85ad174db09e8818a481f
Chen-Yu Pao <windperson@gmail.com>
2022-07-20 14:25:43 +0800

    Runnable on local dev environment

--- src/UrlShortener.Frontend/Program.cs
+++ src/UrlShortener.Frontend/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.AspNetCore.Http.Extensions;
+
+using Orleans;
+using Orleans.Hosting;
+
+using UrlShortener.Backend.Interfaces;
+
 namespace UrlShortener.Frontend;
 
 public class Program
@@ -5,9 +12,32 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Host.UseOrleans(siloBuilder =>
+        {
+            siloBuilder.UseLocalhostClustering();
+            siloBuilder.AddMemoryGrainStorage("url-store");
+        });
+        
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.MapGet("/", () => "Type \"/shorten/{your original url}\" in address bar to get your shorten url.");
+        
+        app.MapMethods("/shorten/{*path}", new []{"GET"}, async (HttpRequest req, IGrainFactory grainFactory, string path) =>
+        {
+            var shortenedRouteSegment = Guid.NewGuid().GetHashCode().ToString("X");
+            var urlStoreGrain = grainFactory.GetGrain<IUrlStoreGrain>(shortenedRouteSegment);
+            await urlStoreGrain.SetUrl(shortenedRouteSegment, path);
+            var resultBuilder = new UriBuilder(req.GetEncodedUrl()) { Path = $"/go/{shortenedRouteSegment}" };
+
+            return Results.Ok(resultBuilder.Uri);
+        });
+        
+        app.MapGet("/go/{shortenUriSegment}", async(string shortenUriSegment, IGrainFactory grainFactory) =>
+        {
+            var urlStoreGrain = grainFactory.GetGrain<IUrlStoreGrain>(shortenUriSegment);
+            var url = await urlStoreGrain.GetUrl();
+            return Results.Redirect(url);
+        });
 
         app.Run();
     }
